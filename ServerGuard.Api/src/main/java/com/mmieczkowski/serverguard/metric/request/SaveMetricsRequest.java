f50f46653d9d107b75ada47700386ab0ba5673c4
Mateusz Mieczkowski <mieczkowski.mateusz@outlook.com>
2024-11-14 20:58:04 +0100

    Remove validation

--- ServerGuard.Api/src/main/java/com/mmieczkowski/serverguard/metric/request/SaveMetricsRequest.java
+++ ServerGuard.Api/src/main/java/com/mmieczkowski/serverguard/metric/request/SaveMetricsRequest.java
@@ -8,7 +8,7 @@ import jakarta.validation.constraints.NotNull;
 import java.time.Instant;
 import java.util.List;
 
-public record SaveMetricsRequest(@NotNull Instant time, @Valid List<SensorMetric> sensorMetrics) {
+public record SaveMetricsRequest(@NotNull Instant time, List<SensorMetric> sensorMetrics) {
     public record SensorMetric(@NotBlank String name, @Valid List<Metric> metrics) {
         public record Metric(@NotBlank String name, float value, @NotNull MetricType type) {
         }
