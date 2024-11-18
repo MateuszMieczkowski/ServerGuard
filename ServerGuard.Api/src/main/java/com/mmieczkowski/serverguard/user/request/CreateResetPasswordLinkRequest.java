988f0feae09a0d33921b2d704649a161ee62c3df
Mateusz Mieczkowski <mieczkowski.mateusz@outlook.com>
2024-11-18 17:49:58 +0100

    Change timezoneId validation

--- ServerGuard.Api/src/main/java/com/mmieczkowski/serverguard/user/request/CreateResetPasswordLinkRequest.java
+++ ServerGuard.Api/src/main/java/com/mmieczkowski/serverguard/user/request/CreateResetPasswordLinkRequest.java
@@ -4,5 +4,5 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
-public record CreateResetPasswordLinkRequest(@NotBlank @Email @Size(min = 5, max=50) String email, @Size(min=1, max = 10) String timeZoneId) {
+public record CreateResetPasswordLinkRequest(@NotBlank @Email @Size(min = 5, max=50) String email, @NotBlank @Size(min=1, max = 32) String timeZoneId) {
 }
