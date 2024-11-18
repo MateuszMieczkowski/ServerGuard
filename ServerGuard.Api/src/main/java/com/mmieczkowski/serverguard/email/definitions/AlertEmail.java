314437b4ae32f389732677d646c550bfc232b266
Mateusz Mieczkowski <mieczkowski.mateusz@outlook.com>
2024-11-18 17:33:41 +0100

    Fix AlertEmail template

--- ServerGuard.Api/src/main/java/com/mmieczkowski/serverguard/email/definitions/AlertEmail.java
+++ ServerGuard.Api/src/main/java/com/mmieczkowski/serverguard/email/definitions/AlertEmail.java
@@ -18,7 +18,7 @@ public class AlertEmail extends EmailDefinition {
     }
     @Override
     public String getTemplateFileName() {
-        return "resetPasswordEmail";
+        return "alertEmailTemplate";
     }
 
     @Override
