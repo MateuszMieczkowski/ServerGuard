d77827ad23607f78d00b453c46e25bce77002041
Mateusz Mieczkowski <mieczkowski.mateusz@outlook.com>
2025-01-11 12:21:06 +0100

    Add env files for api urls

--- ServerGuard.Web/src/config.ts
+++ ServerGuard.Web/src/config.ts
@@ -1,13 +1,11 @@
-import { apiUrl, webSocketUrl } from '../config.json';
-
 interface Config {
   apiUrl: string;
   webSocketUrl: string;
 }
 
 const config: Config = {
-  apiUrl: apiUrl,
-  webSocketUrl: webSocketUrl
+  apiUrl: import.meta.env.VITE_API_URL || "http://localhost:8080/api",
+  webSocketUrl: import.meta.env.VITE_WEBSOCKET_URL || "ws://localhost:8080/ws",
 };
 
 const getConfig = () => config;
